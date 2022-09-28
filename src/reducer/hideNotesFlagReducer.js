424d2740e89bab1e26a0d5456c5b6c66ff48992c
Mathi21as <65058477+Mathi21as@users.noreply.github.com>
2022-09-28 12:10:14 -0300

    Delete src directory

--- src/reducer/hideNotesFlagReducer.js
+++
@@ -1,9 +0,0 @@
-export const hideNotesFlagReducer = (state, action) => {
-  switch(action.flag){
-    case false: return {id: action.id, flag: false}
-
-    case true: return {id: action.id, flag: true}
-
-    default: return state
-  }
-}
